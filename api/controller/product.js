cc3e317cb1f9b9ab5b93d395ebe4a1aa73d7ecb4
OlwethuB <olwethu.a.bhiya@gmail.com>
2023-08-23 11:25:31 +0200

    Do

--- api/controller/product.js
+++ api/controller/product.js
@@ -1,61 +1,76 @@
-    // importing functions from the product model
-    const Product = require("../model/productModel");
-const product = new Product()
-    // Get all products
-    exports.showProducts = (req, res) => {
-        product.getProducts((err, results) => {
-            if (err){
-                res.send(err);
-            } else{
-                res.json(results)
-            }
-        })
+// importing functions from the product model
+const Product = require("../model/productModel");
+const product = new Product();
+// Get all products
+exports.showProducts = (req, res) => {
+  product.getProducts((err, results) => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.json({
+        status: res.statusCode,
+        results,
+      });
     }
-    
-    //  Get single product 
-    exports.getProductId = (req, res) => {
-        product.getProductId(req.params.id, (err,result) =>{
-            if (err){
-                res.send(err);
-            } else{
-                res.json(result);
-            }
-        })
-    }
+  });
+};
 
-    // create a new product
-    exports.createProduct = (req, res) => {
-        const data = req.body;
-        product.insertProduct(data, (err, results) =>{
-            if (err){
-                res.send(err);
-            } else{
-                res.json(results);
-            }
-        })
+//  Get single product
+exports.getProductId = (req, res) => {
+  product.getProductId(req.params.id, (err, result) => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.json({
+        status: res.statusCode,
+        result,
+      });
     }
+  });
+};
 
-    // update product
+// create a new product
+exports.createProduct = (req, res) => {
+  const data = req.body;
+  product.insertProduct(data, (err, results) => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.json({
+        status: statusCode,
+        msg: "Product has been added",
+      });
+    }
+  });
+};
+
+// update product
 exports.updateProduct = (req, res) => {
-    const data = req.body;
-    const id = req.params.id;
-    product.updateProductById(data, id, (err, results) => {
-        if (err){
-            res.send(err);
-        } else{
-            res.json(results);
-        }
-    })
-}
+  const data = req.body;
+  const id = req.params.id;
+  product.updateProductById(data, id, (err, results) => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.json({
+        status: res.statusCode,
+        msg: "This product record has been updated",
+      });
+    }
+  });
+};
 
-// delete product 
+// delete product
 exports.deleteProduct = (req, res) => {
-    const id = req.params.id;
-    product.deleteProductById(id, (err, results) => {
-        if (err){
-            res.send(err);
-        } else{
-            res.json(results);
-        }
-    })
-}
+  const id = req.params.id;
+  product.deleteProductById(id, (err, results) => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.json({
+        status: res.statusCode,
+        msg: "Product record deleted",
+      });
+    }
+  });
+};
